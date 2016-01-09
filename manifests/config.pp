91bd1ea6555f94e74090fdc1b58e996fc22aea3c
Frédéric DROUET <fdrouet@exoplatform.com>
2016-01-09 23:26:12 +0100

    Change the way to configure (using file_line)

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,22 +6,40 @@ class apt_cacher_ng::config {
     path   => $apt_cacher_ng::params::cache_dir,
     mode   => $apt_cacher_ng::directory_mode,
   } -> # Configure apt-cacher-ng to enforce umask and directory + file permissions
-  file { '/etc/default/apt-cacher-ng':
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 644,
-    content => template("${module_name}/etc/default/apt-cacher-ng.erb"),
+  file_line { 'apt-cacher-ng-default_umask':
+    ensure => present,
+    path   => '/etc/default/apt-cacher-ng',
+    line   => 'umask 000',
+    match  => '^umask ',
+    notify  => Service['apt-cacher-ng'],
+  } ->
+  file_line { 'apt-cacher-ng-default_daemon':
+    ensure => present,
+    path   => '/etc/default/apt-cacher-ng',
+    line   => "DAEMON_OPTS=' -c ${apt_cacher_ng::params::configuration_dir} '",
+    match  => '^DAEMON_OPTS=',
     notify  => Service['apt-cacher-ng'],
   } -> # apt-cacher-ng Configuration
-  file { '/etc/apt-cacher-ng/acng.conf':
-    ensure  => present,
-    owner   => root,
-    group   => root,
-    mode    => 644,
-    content => template("${module_name}/etc/apt-cacher-ng/acng.conf.erb"),
-    require => Package[$apt_cacher_ng::params::package_name],
-    notify  => Service[$apt_cacher_ng::params::service_name],
+  file_line { 'apt-cacher-ng_cache-dir':
+    ensure => present,
+    path   => '/etc/apt-cacher-ng/acng.conf',
+    line   => "CacheDir:${apt_cacher_ng::params::cache_dir}",
+    match  => '^CacheDir:',
+    notify  => Service['apt-cacher-ng'],
+  } ->
+  file_line { 'apt-cacher-ng_log-dir':
+    ensure => present,
+    path   => '/etc/apt-cacher-ng/acng.conf',
+    line   => "LogDir:${apt_cacher_ng::params::log_dir}",
+    match  => '^LogDir:',
+    notify  => Service['apt-cacher-ng'],
+  } ->
+  file_line { 'apt-cacher-ng_port':
+    ensure => present,
+    path   => '/etc/apt-cacher-ng/acng.conf',
+    line   => "Port:${apt_cacher_ng::params::port}",
+    match  => '^Port:',
+    notify  => Service['apt-cacher-ng'],
   }
 
 }
